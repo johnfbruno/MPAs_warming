e24985bab83596de0fab4464ba61f96ff242045e
markruddy <drmarkruddy@gmail.com>
2017-04-21 18:55:49 +0100

    Plot updates
    Viridis, plot extent, raster data rotation, colour bar breaks

--- revrotate..R
+++ revrotate..R
@@ -4,13 +4,16 @@ revrotate<-function (x, ...)
   {
     e <- extent(x)
     ext1 <- extent(-180, 0, -90, 90)
+
     if (is.null(intersect(e, ext1))) {
       r1 <- NULL
     }
     else {
       r1 <- crop(x, ext1)
     }
+
     ext2 <- extent(0, 180 + res(x)[1], -90, 90)
+
     if (is.null(intersect(e, ext2))) {
       r2 <- NULL
     }
@@ -18,7 +21,9 @@ revrotate<-function (x, ...)
       r2 <- crop(x, ext2)
       r2 <- shift(r2, -360)
     }
+
     ln <- names(x)
+
     if (is.null(r1)) {
       out <- r2
     }
@@ -28,18 +33,22 @@ revrotate<-function (x, ...)
     else {
       out <- merge(r1, r2, overlap = FALSE)
     }
+
     names(out) <- names(x)
     out@z <- x@z
     p <- projection(out)
     extent(out)<-c(0,360,ymin(x),ymax(x))
+    
     if (length(grep("\\+over", p)) > 0) {
       projection(out) <- gsub("[[:space:]]\\+over", "", 
                               p)
     }
+
     if (filename != "") {
       out <- writeRaster(out, filename, ...)
     }
     return(out)
   }
+
   .local(x, ...)
 }
